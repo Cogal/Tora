8c080ce1c980d81da55c8a8ddfef9a2a37c66ece
Cogal <rio.mathieu20@gmail.com>
2020-05-22 20:52:48 +0200

    ajout des bases html, css, js

---
+++ connect.js
@@ -0,0 +1,18 @@
+var mpd;
+var pseudo;
+var valeur = 0;
+
+// boucle 
+while (valeur < 1){
+
+// demande des identifiants
+var mdp = prompt(' Mots de passe');
+var pseudo = prompt("Pseudo");
+if (mdp === "admin" && pseudo ==="admin"){
+    valeur++;
+}
+
+};
+
+
+
